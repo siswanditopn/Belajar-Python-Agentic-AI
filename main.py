416f445105be8500f7e1cac92ec0aa6e6242b2aa
siswanditopn <siswanditopn@gmail.com>
2026-09-22 21:45:13 +0900

    (Fase 2) Deploy to Production - Konsep Webhook

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+from loguru import logger
 from fastapi import FastAPI, Request, Response, status
 
 app = FastAPI()
 
-@app.get("/testing-root")
+
+@app.get("/")
 async def root():
-    return {"status": "ok"}
+    return {"status": "ini dari root"}  # json
+
+
+@app.get("/get")
+async def endpoint_get():
+    return {"http_method": "get"}
+
+
+# http request -> get dan post
+@app.post("/post")
+async def endpoint_post():
+    return {"http_method": "post"}
+
+
+@app.post("/webhook")
+async def telegram_webhook(request: Request):
+    req_body = await request.json()
+    logger.debug(req_body)
+    return Response(status_code=status.HTTP_200_OK)
